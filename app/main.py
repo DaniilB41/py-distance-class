5336221313ec4bc1464d24069bd55d7fed7a7406
Daniil Baianov <bayanovdanil666@gmail>
2025-12-22 14:50:20 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,72 @@
+from __future__ import annotations
+
+
+def magic_test(func):
+    def inner(self, other: object) -> bool:
+        if isinstance(other, Distance):
+            rhs = other.km
+        elif isinstance(other, (int, float)):
+            rhs = other
+        else:
+            return NotImplemented
+        return func(self, rhs)
+
+    return inner
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            value = other.km
+        elif isinstance(other, (int, float)):
+            value = other
+        else:
+            return NotImplemented
+        return Distance(self.km + value)
+
+    def __iadd__(self, other: Distance) -> Distance:
+        if isinstance(other, Distance):
+            value = other.km
+        elif isinstance(other, (int, float)):
+            value = other
+        else:
+            return NotImplemented
+        self.km += value
+        return self
+
+    def __mul__(self, other: int | float) -> Distance:
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: int | float) -> Distance:
+        if other == 0:
+            raise ZeroDivisionError("division by zero")
+        return Distance(round(self.km / other, 2))
+
+    @magic_test
+    def __lt__(self, rhs: int | float) -> bool:
+        return self.km < rhs
+
+    @magic_test
+    def __gt__(self, rhs: int | float) -> bool:
+        return self.km > rhs
+
+    @magic_test
+    def __eq__(self, rhs: int | float) -> bool:
+        return self.km == rhs
+
+    @magic_test
+    def __le__(self, rhs: int | float) -> bool:
+        return self.km <= rhs
+
+    @magic_test
+    def __ge__(self, rhs: int | float) -> bool:
+        return self.km >= rhs
